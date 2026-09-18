ec2bcc358ea96cd0f0823062c2f65ada833d6567
Aayushtamang <jimba.lama22@gmail.com>
2026-09-18 17:27:15 +0545

    Hero Section Created with Animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import HeroSection from './sections/HeroSection'
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <HeroSection />
     </>
   )
 }
