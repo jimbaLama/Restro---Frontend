8384b15274ddcf3931303a0bd4b6735d8ecd6675
Aayushtamang <jimba.lama22@gmail.com>
2026-09-18 18:54:41 +0545

    About and Dishes Section Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './sections/HeroSection'
+import About from './sections/About'
+import Stats from './sections/Stats'
+import Dishes from './sections/Dishes'
 
 const App = () => {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <About />
+      <Stats />
+      <Dishes />
     </>
   )
 }
