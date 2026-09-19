582de3888173325baeab444cf089178f21e5eabf
Aayushtamang <jimba.lama22@gmail.com>
2026-09-19 08:54:57 +0545

    Features Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HeroSection from './sections/HeroSection'
 import About from './sections/About'
 import Stats from './sections/Stats'
 import Dishes from './sections/Dishes'
+import Features from './sections/Features'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <About />
       <Stats />
       <Dishes />
+      <Features />
     </>
   )
 }
